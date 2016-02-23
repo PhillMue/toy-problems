066f2cf7e489d3868fa3d327faf974cf4f516b2f
PhillMue <muenzuve@gmail.com>
2016-02-23 09:51:48 +0300

    common element

---
+++ common_element/src/commonelement/Commonelement.java
@@ -0,0 +1,23 @@
+// Using Java, come up with an algorithm to identify common elements or 
+// numbers between two given arrays. Do NOT use any inbuilt methods.
+
+
+
+package commonelement;
+
+public class Commonelement {
+
+	
+	public static void main(String a[]) {
+		int[] arrayA = {0, 1, 4, 7, 9, 11};
+		int[] arrayB = {2, 4, 6, 9, 10, 14};
+		for(int i = 0; i<arrayA.length;i++){
+		     for(int j=0;j<arrayB.length;j++){
+		     if(arrayA[i]==arrayA[j]){
+		     System.out.println(arrayA[i]);]
+		                }
+		            }
+			}
+		}
+
+}
